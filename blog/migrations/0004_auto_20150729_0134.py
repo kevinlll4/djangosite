fca5eba10f73b4eb29d39d545ca73ee13314a937
kevin <escorpio_2_1994@hotmail.com>
2015-07-30 12:29:18 -0500

    mi primer commit en django

---
+++ blog/migrations/0004_auto_20150729_0134.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0003_auto_20150729_0111'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='created_date',
+            field=models.DateTimeField(default=django.utils.timezone.now),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='published_date',
+            field=models.DateTimeField(null=True, blank=True),
+        ),
+    ]
